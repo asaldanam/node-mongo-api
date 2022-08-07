080e33409db096f34ef921d5a2cff258484bddd4
asaldanam <asaldanam@gmail.com>
2022-08-07 18:13:10 +0200

    cors

--- src/app.js
+++ src/app.js
@@ -3,19 +3,18 @@ import express from 'express'
 import { changePasswordController } from './controllers/change-password.js';
 import { loginController } from './controllers/login.js';
 import { registerController } from './controllers/register.js';
+import cors from 'cors'
 
 const app = express();
 const port = 8081;
 
-app.listen(port, () => {
-  console.log(`Aplicación correctamente levantada en el puerto ${port} 🥳 🥳 🥳 🥳 `)
-})
+app.listen(port, () => console.log(`Running in ${port}`))
 
 app.use(express.json())
-
+app.use(cors())
 
 // routes
-app.get('/', (req, res) => { res.send({ msg: 'Hello World' }) })
+app.get('/', (req, res) => { res.send({ message: 'node-mongo-api works!' }) })
 app.post('/login', loginController)
 app.post('/register', registerController)
 app.post('/change-password', changePasswordController)
